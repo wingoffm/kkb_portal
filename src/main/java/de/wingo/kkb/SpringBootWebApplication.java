1162a1c8a0047a65cfbb6df3b43f8f03e8d74dc8
wingoffm <winfriedgottschalk@t-online.de>
2020-02-01 19:40:55 +0100

    Changing Java version to 11.

--- src/main/java/de/wingo/kkb/SpringBootWebApplication.java
+++ src/main/java/de/wingo/kkb/SpringBootWebApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package de.wingo.kkb;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class SpringBootWebApplication {
 
 	@GetMapping("/")
 	String home() {
@@ -14,6 +14,6 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 }
