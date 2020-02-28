5f93938add2bb1f849f706316a4421627486dffe
wingoffm <winfriedgottschalk@t-online.de>
2020-02-28 09:56:16 +0100

    Initial

--- src/main/java/de/wingo/kkb/SpringBootWebApplication.java
+++ src/main/java/de/wingo/kkb/SpringBootWebApplication.java
@@ -1,8 +1,9 @@
 package de.wingo.kkb;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USERS (user_name, password) VALUES
+  ('Lokesh', 'plaUR324'),
+  ('Deja', '1UhngRe#+'),
+  ('Caption', 'z@34RDpo8#');
